83060143164a40a0f7b3e8cc51c5b3642bee6f1c
mac <408200244@qq.com>
2019-01-01 02:56:09 +0800

    第二次git提交

--- manager-mapper/src/main/java/com/ck/App.java
+++ manager-mapper/src/main/java/com/ck/App.java
@@ -7,7 +7,9 @@ package com.ck;
 public class App {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        System.out.println("111");
     }
 
 }
